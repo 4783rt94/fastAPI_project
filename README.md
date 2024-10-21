038c3dbfcf1ea6ca2e9854850b24355d208fca06
Albert Tokarski <tokarskialbert@proton.me>
2024-10-21 20:29:11 +0200

    User model added, two endpoints added

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # fastAPI_project
-An example project demonstrating the use of the FastAPI framework for building efficient and scalable APIs in Python. The repository contains basic code showcasing how to quickly set up an API with built-in documentation and asynchronous support. Ideal as a starting point for learning FastAPI.
+An example project demonstrating the use of the FastAPI framework for building efficient and scalable APIs in Python. The repository contains basic code showcasing how to quickly set up an API with built-in documentation and asynchronous support.
--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 from fastapi import FastAPI
+from models import User, Gender, Role
+from typing import List
+from uuid import UUID, uuid4
 
 app = FastAPI()
 
+# Temporary list simulating a database
+db: List[User] = [
+    User(id=UUID("9b331d15-0130-4f31-9dc3-1209ac057408"), first_name="Mikael", last_name="Smith", gender=Gender.male, roles=[Role.student]),
+    User(id=UUID("7415ba23-3ad4-4e5f-b1ea-bca1b58aa5ac"), first_name="Alice", last_name="Terry", gender=Gender.female, roles=[Role.admin, Role.user])
+]
+
 @app.get("/")
-def read_root():
-    return {"message": "Hello Word"}
+async def root():
+    return {"Hello": "World!"}
+
+@app.get("/api/users")
+async def fetch_users():
+    return db
+
+@app.post("/api/users/register")
+async def register_user(user: User):
+    db.append(user)
+    return {"id": user.id}
 
